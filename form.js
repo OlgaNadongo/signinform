584e70dabf44b14429df550b45f69c081bbd1d39
Olga Nadongo <winnienadongo@gmail.com>
2023-05-15 21:15:21 +0300

    adding onclick event  listener on create account button

---
+++ form.js
@@ -0,0 +1,18 @@
+function clickbutton(){
+    document.getElementById("btn").style.color='green';
+}
+
+function myfunction(){
+    var text1="Hello"
+    var text2="Citizens"
+    var text3= text1+" "+text2
+
+    document.getElementById("demo").innerHTML=text3
+}
+
+function loggedinside(){
+    var greeting1="Welcome back,"
+    var greetingname="Olga"
+
+    document.getElementById("loggedin").innerHTML=greeting1+" "+greetingname
+}
